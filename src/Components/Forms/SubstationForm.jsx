a5297715dfade5ba6eda1d555a80138c75f5b8af
LuisMFG <luismiguelfloresgr@gmail.com>
2023-01-10 19:27:25 +0100

    Cleaning folder structure

--- src/Components/Forms/SubstationForm.jsx
+++ src/Components/Forms/SubstationForm.jsx
@@ -1,111 +1,53 @@
-import { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
-import { dataBase, equipments } from "../../../utils";
-import InputSelect from "../Inputs/InputSelect";
-import { downLoadFirebaseStorage } from "../../../utils";
+import { inputCreator, getOptions } from "../../../utils";
+import { useForm } from "react-hook-form";
 
 const { Group } = Form;
 
 function SubstationForm() {
-  const [formValues, setFormValues] = useState({
-    substation: "",
-    section: "",
-    equipment: "",
-  });
+  const { register, handleSubmit, watch } = useForm();
 
-  const inputValueHandler = (e) => {
-    const { name, value } = e.target;
-    setFormValues((prev) => ({ ...prev, [name]: value }));
-  };
+  const lastSubstationSelected = watch("substation");
+  const lastSectionSelected = watch("section");
 
-  const submitHandler = (e) => {
-    e.preventDefault();
-
-    downLoadFirebaseStorage(
-      `${formValues.substation}/${formValues.section}/${formValues.substation}_${formValues.section}_${formValues.equipment}.pdf`
-    );
-  };
-
-  const isAnyInputNotSelected = () => {
-    let anyInputNotSelected = true;
-
-    if (
-      formValues.substation !== "" &&
-      formValues.section !== "" &&
-      formValues.equipment !== ""
-    ) {
-      anyInputNotSelected = false;
-    }
-    return anyInputNotSelected;
-  };
-
-  const substationOptions = dataBase.map((element) => {
-    return (
-      <option key={element.id} value={element.id}>
-        {element.id} - {element.substation}
-      </option>
-    );
-  });
-
-  const equipmentOptions =
-    formValues.section &&
-    equipments.map((element) => {
-      return (
-        <option key={element.value} value={element.value}>
-          {element.name}
-        </option>
-      );
-    });
-
-  const sectionOptions =
-    formValues.substation &&
-    dataBase
-      .find((element) => element.id === formValues.substation)
-      .sections.map((element) => (
-        <option key={element} value={element}>
-          Secc. {element}
-        </option>
-      ));
+  const inputsScheme = [
+    {
+      register,
+      label: "Substación",
+      name: "substation",
+      isRequired: true,
+      placeholder: "Elige una subestación",
+      options: getOptions(null),
+    },
+    {
+      register,
+      label: "Sección",
+      name: "section",
+      isRequired: true,
+      placeholder: "Elige una sección",
+      options: getOptions(lastSubstationSelected),
+    },
+    {
+      register,
+      label: "Equipos",
+      name: "equipment",
+      isRequired: true,
+      placeholder: "Elige un equipo",
+      options: getOptions(lastSectionSelected),
+    },
+  ];
 
   return (
-    <Form onSubmit={submitHandler}>
+    <Form onSubmit={handleSubmit((data) => console.log(data))}>
       <Group className="mb-3">
-        <InputSelect
-          label="Substatión"
-          name="substation"
-          inputValueHandler={inputValueHandler}
-          placeholder="Elige una subestación"
-          dinamicOptions={substationOptions}
-        />
-      </Group>
-
-      <Group className="mb-3">
-        <InputSelect
-          label="Sección"
-          name="section"
-          inputValueHandler={inputValueHandler}
-          placeholder="Elige una subestación"
-          dinamicOptions={sectionOptions}
-        />
-      </Group>
-
-      <Group className="mb-3">
-        <InputSelect
-          label="Equipo"
-          name="equipment"
-          inputValueHandler={inputValueHandler}
-          placeholder="Elige un equipo"
-          dinamicOptions={equipmentOptions}
-        />
+        {inputsScheme.map((element) => {
+          return inputCreator(element);
+        })}
       </Group>
 
       <div className="d-flex justify-content-center ">
-        <Button
-          type="submit"
-          variant={isAnyInputNotSelected() ? "secondary" : "primary"}
-          disabled={isAnyInputNotSelected()}
-        >
+        <Button type="submit" variant="primary">
           Descargar plano
         </Button>
       </div>
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,12 +1,12 @@
 import Container from "react-bootstrap/Container";
 import Header from "../Header/Header";
-import SubstationForm2 from "../Forms/SubstationForm2";
+import SubstationForm from "../Forms/SubstationForm";
 
 const mobile = (
   <div className="maxWidthForMobile mh-50 bg-white roundedTwoRem p-0 shadow mx-4">
     <Header />
     <Container className="p-4">
-      <SubstationForm2 className="bg-light" />
+      <SubstationForm className="bg-light" />
     </Container>
   </div>
 );
