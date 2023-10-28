1337158bd80bfac301f049cf4bbf98a143d80abf
LuisMFG <luismiguelfloresgr@gmail.com>
2023-10-28 20:14:35 +0200

    bug state input select

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,6 @@ import { authContext } from '../context/AuthContext'
 import { useNavigate } from 'react-router-dom'
 import InputSelect from '../components/InputSelect'
 import useSubstations from '../hooks/useSubstations'
-import useResize from '../hooks/useResize'
 
 const Home = () => {
   const { userLogOut } = useContext(authContext)
@@ -20,23 +19,23 @@ const Home = () => {
 
   const handleOnchange = (e) => {
     const { name, value } = e.target
+    const newValue = value.includes('Select') ? null : value
     setSelectedFields((prev) => {
-      return { ...prev, [name]: value }
+      return { ...prev, [name]: newValue }
     })
   }
 
   const getSections = (value) => {
     const obj = data.find((element) => element.value === value)
-    const sectionsOptions = Object.keys(obj.sections)
-    return sectionsOptions
+    return obj ? Object.keys(obj.sections) : []
   }
 
   const getEquipment = () => {
     const substationObj = data.find(
       (element) => element.value === selectedFields.substation
     )
-    const equipmentObj = substationObj.sections[selectedFields.section]
-    return Object.keys(equipmentObj)
+    const equipmentObj = substationObj?.sections[selectedFields.section]
+    return equipmentObj ? Object.keys(equipmentObj) : []
   }
 
   const onSubmit = (e) => {
@@ -52,69 +51,76 @@ const Home = () => {
     {
       name: 'substation',
       label: 'Substation',
+      state: selectedFields.substation,
       options: data,
-      handleOnchange,
-      disabled: false
+      handleOnchange
     },
     {
       name: 'section',
       label: 'Section',
+      state: selectedFields.section,
       options: selectedFields.substation
         ? getSections(selectedFields.substation)
         : [],
-      handleOnchange,
-      disabled: !selectedFields.substation
+      handleOnchange
     },
     {
       name: 'equipment',
       label: 'Equipment',
+      state: selectedFields.equipment,
       options: selectedFields.section
         ? getEquipment(selectedFields.section)
         : [],
-      handleOnchange,
-      disabled: !selectedFields.section
+      handleOnchange
     }
   ]
 
+  const allInputsSelected = (obj) => {
+    return Object.values(obj).every((value) => value !== null)
+  }
+
   return (
-    <section>
-      <div className='hStack h-[80vh] w-[1200px] items-center gap-10 rounded-xl bg-white p-10 shadow-2xl'>
+    <section className='vStack max-w-[1000px]'>
+      <div className='hStack gap-10 rounded-xl bg-white p-5 shadow-2xl'>
         <h1 className='text-center text-[3rem] font-bold text-gray-600'>
           Blueprint Manager
         </h1>
-        <form
-          className='hStack items-center gap-10'
-          onSubmit={(e) => onSubmit(e)}
-        >
-          <div className='vStack gap-10'>
-            {inputSchema.map(
-              ({ name, label, options, handleOnchange, disabled }) => (
-                <InputSelect
-                  key={name}
-                  name={name}
-                  label={label}
-                  options={options}
-                  handleOnchange={handleOnchange}
-                  disabled={disabled}
-                />
-              )
-            )}
-          </div>
+        <div className='hStack items-center gap-4'>
+          <form
+            className='hStack items-center gap-10'
+            onSubmit={(e) => onSubmit(e)}
+          >
+            <div className='vStack gap-10'>
+              {inputSchema.map(
+                ({ name, label, state, options, handleOnchange, disabled }) => (
+                  <InputSelect
+                    key={name}
+                    name={name}
+                    state={state}
+                    label={label}
+                    options={options}
+                    handleOnchange={handleOnchange}
+                    disabled={disabled}
+                  />
+                )
+              )}
+            </div>
+            <Button
+              type='submit'
+              text={'Open blueprint'}
+              color={'blue'}
+              disabled={!allInputsSelected(selectedFields)}
+            />
+          </form>
           <Button
-            type='submit'
-            text={'Open blueprint'}
-            color={'blue'}
-            disabled={!selectedFields.equipment}
+            text='Log Out'
+            color='red'
+            callBack={() => {
+              userLogOut(auth)
+              navigate('/')
+            }}
           />
-        </form>
-        <Button
-          text='Log Out'
-          color='red'
-          callBack={() => {
-            userLogOut(auth)
-            navigate('/')
-          }}
-        />
+        </div>
       </div>
     </section>
   )
